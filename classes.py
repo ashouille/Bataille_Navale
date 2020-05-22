34bec667acbf7a457c6fb8d7e5b0310722def22a
Alex <alexandre.crevel@outlook.com>
2020-05-22 18:45:52 +0200

    rework main

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 import random
 
 
-def has_won(tab):
+def finished(tab):
     ships_remaining = 5
     for element in tab:
         if element.is_destroyed:
@@ -14,25 +14,24 @@ def has_won(tab):
 class Plateau:
     def __init__(self, size):
         self.size = size
-        self.tab = [[' '] * size for _ in range(size)]
+        self.tab = [[" "] * size for _ in range(size)]
         self.x_pos = 0
         self.y_pos = 0
         self.horizontal = False
 
     def print_tab(self):
-        first_line = '  | '
+        first_line = "  | "
         for number in range(self.size):
             if number >= 10:
-                first_line = first_line + str(number) + '| '
+                first_line = first_line + str(number) + "| "
             else:
-                first_line = first_line + str(number) + ' | '
+                first_line = first_line + str(number) + " | "
         print(first_line)
 
-        point_x = 0
-        letter = 'A'
-        while point_x < self.size:
-            line = str(letter) + ' | ' + ' | '.join(self.tab[point_x]) + ' |'
-            point_x += 1
+
+        letter = "A"
+        for point_x in range(self.size):
+            line = str(letter) + " | " + " | ".join(self.tab[point_x]) + " |"
             letter = chr(ord(letter[0]) + 1)
             print(line)
 
@@ -41,28 +40,31 @@ class Plateau:
         # Test si la chaine de caractère d'entrée est valide (forme : B2H par exemple)
         # Si True, defini les variables x_pos, y_pos et horizontal
         if (
-                len(inp) == 3
-                and inp[0].upper() in 'ABCDEFGHIJ'
-                and inp[1].isdigit()
-                and inp[2].upper() in 'HV'
+            len(inp) == 3
+            and inp[0].upper() in "ABCDEFGHIJ"
+            and inp[1].isdigit()
+            and inp[2].upper() in "HV"
         ):
             self.y_pos = ord(inp[0].upper()) - 65
             self.x_pos = int(inp[1])
             self.horizontal = False
-            if inp[2].upper() == 'H':
+            if inp[2].upper() == "H":
                 self.horizontal = True
         else:
             return False
 
         # Si la taille du navire dépasse du plateau, retourne False
-        if (self.horizontal and self.x_pos + boat.lenght >= self.size or
-            not self.horizontal and self.y_pos + boat.lenght >= self.size
+        if (
+            self.horizontal
+            and self.x_pos + boat.lenght > self.size
+            or not self.horizontal
+            and self.y_pos + boat.lenght > self.size
         ):
             return False
 
         # Pour chaque coordonnée du navire, ajoute les caractère haut, bas
         # gauche et droite dans ship_around
-        ship_around = ''
+        ship_around = ""
         for elt in range(boat.lenght):
 
             if self.horizontal:
@@ -84,7 +86,7 @@ class Plateau:
                 ship_around += self.tab[temp_y + 1][temp_x]
 
         # Si aucun caractère n'est trouvé dans ship_around, retourne True
-        for element in 'PCVSR':
+        for element in "PCVSR":
             if element in ship_around:
                 return False
         return True
@@ -108,9 +110,9 @@ class Plateau:
             is_good = False
             while not is_good:
                 spot = (
-                        random.choice('ABCDEFGHIJ')
-                        + str(random.randint(0, 9))
-                        + random.choice('HV')
+                    random.choice("ABCDEFGHIJ")
+                    + str(random.randint(0, 9))
+                    + random.choice("HV")
                 )
                 is_good = self.place_ship(spot, boat_list[element])
                 if is_good:
@@ -119,30 +121,35 @@ class Plateau:
     def _spot_is_valid(self, inp):
         # Teste si la position de tir est valide
         if len(inp) == 2:
-            if (inp[0].upper() in 'ABCDEFGHIJ'
-                    and inp[1].isdigit()
-                    and self.tab[self.y_pos][self.x_pos] == ' '
+            if (
+                inp[0].upper() in "ABCDEFGHIJ"
+                and inp[1].isdigit()
+                and self.tab[self.y_pos][self.x_pos] == " "
             ):
                 self.y_pos = ord(inp[0].upper()) - 65
                 self.x_pos = int(inp[1])
                 return True
         return False
 
-    def play_position(self, computer_tab, inp):
+    def play_position(self, computer_tab, inp, ship_list):
         if self._spot_is_valid(inp):
-            if computer_tab[self.y_pos][self.x_pos] in 'PCVSR':
+            if computer_tab[self.y_pos][self.x_pos] in "PCVSR":
                 char = computer_tab[self.y_pos][self.x_pos]
-                computer_tab[self.y_pos][self.x_pos] = 'T'
-                self.tab[self.y_pos][self.x_pos] = 'T'
-                print('Touché !')
-                return char, True
+                for element in ship_list:
+                    if char == element.char:
+                        element.is_touched()
+                computer_tab[self.y_pos][self.x_pos] = "T"
+                self.tab[self.y_pos][self.x_pos] = "T"
+                print("Touché !")
+                return True
 
-            if computer_tab[self.y_pos][self.x_pos] == ' ':
-                self.tab[self.y_pos][self.x_pos] = 'X'
+            if computer_tab[self.y_pos][self.x_pos] == " ":
+                self.tab[self.y_pos][self.x_pos] = "X"
                 print("Dans l'eau !")
-                return ' ', True
-        print('Saisie invalide')
-        return ' ', False
+                return " ", True
+        print("Saisie invalide, recommencez")
+        return " ", False
+
 
 class Ship:
     def __init__(self, name, lenght, char, number=0, is_destroyed=False, shot=0):
@@ -157,7 +164,4 @@ class Ship:
         self.shot += 1
         if self.shot == self.lenght:
             self.is_destroyed = True
-            print(f'Vous avez coulé le {self.name} adverse !')
-
-    def get_is_destroyed(self):
-        return self.is_destroyed
+            print(f"Vous avez coulé le {self.name} adverse !")
